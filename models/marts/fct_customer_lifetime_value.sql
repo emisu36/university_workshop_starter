e4841b0204b0bbf7b61ff154fdd03bd4599721d6
emsu3617 <emilysu.pj@gmail.com>
2026-05-18 15:22:19 -0400

    final submission

--- models/marts/fct_customer_lifetime_value.sql
+++ models/marts/fct_customer_lifetime_value.sql
@@ -2,35 +2,45 @@ with customers as (
     select * from {{ ref('stg_customers') }}
 ),
 
-order_products as (
-    select * from {{ ref('int_customer_orders_products') }}
+orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+items as (
+    select * from {{ ref('stg_items') }}
+),
+
+products as (
+    select * from {{ ref('stg_products') }}
 ),
 
 customer_orders as (
     select
-        customer_id,
-        count(distinct order_id)        as total_orders,
-        sum(order_total)                as lifetime_value,
-        avg(order_total)                as avg_order_value,
-        min(order_date)                 as first_order_date,
-        max(order_date)                 as last_order_date
-    from order_products
+        o.customer_id,
+        count(distinct o.order_id)           as total_orders,
+        sum(o.order_total)                   as lifetime_value,
+        avg(o.order_total)                   as avg_order_value,
+        min(o.order_date)                    as first_order_date,
+        max(o.order_date)                    as last_order_date
+    from orders o
     group by 1
 ),
 
 customer_products as (
     select
-        customer_id,
-        product_type,
-        count(*)                        as items_purchased
-    from order_products
+        o.customer_id,
+        p.product_type,
+        count(*)                             as items_purchased
+    from orders o
+    join items i on i.order_id = o.order_id
+    join products p on p.sku = i.sku
     group by 1, 2
 ),
 
 favourite_product_type as (
     select
         customer_id,
-        product_type                    as favourite_product_type
+        product_type                         as favourite_product_type
     from customer_products
     qualify row_number() over (
         partition by customer_id
